1700fa265cb739e81d82c91cdb6c0b0b1231f43e
Ivy Mendoza <ivy.mendoza@rise-x.io>
2023-05-24 11:24:31 +0800

    Add project files.

---
+++ TaskManagerApp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Transactions;
+using TaskManagerApp;
+
+class Program
+{
+    static List<Task> tasks = new List<Task>();
+    static User currentUser;
+
+    static void Main(string[] args)
+    {
+        ShowWelcomeScreen();
+        Login();
+        ShowMainMenu();
+    }
+
+    static void ShowWelcomeScreen()
+    {
+        Console.WriteLine("Welcome to My Task Manager Application!");
+        Console.WriteLine("***************************************");
+        Console.WriteLine();
+    }
+
+    static void Login()
+    {
+        Console.WriteLine("Enter your UserName and Password to log in.");
+        Console.WriteLine("UserName: ");
+        string username = Console.ReadLine();
+        Console.Write("Password: ");
+        string password = Console.ReadLine();
+
+        if (username == "admin"  && password == "password") 
+        {
+            currentUser = new User { UserName = username, Password = password};
+            Console.WriteLine("Login Successful!!");
+            Console.WriteLine();
+        }
+
+        else
+        {
+            Console.WriteLine("INVALID USERNAME OR PASSWORD. EXITING THE APPLICATION");
+            Environment.Exit(0);
+        }
+    }
+
+}
---
+++ TaskManagerApp/Task.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagerApp
+{
+    internal class Task
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public string Status { get; set; }
+        public string Priority { get; set; }
+    }
+
+    
+}
